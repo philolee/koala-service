6448c38347a58daeb055786fae99ededcdcbd28d
Yang Li <philolee35@gmail.com>
2014-12-28 14:00:40 +0800

    Add mybatis generator.

--- src/main/java/com/koala/model/FinanceProduct.java
+++ src/main/java/com/koala/model/FinanceProduct.java
@@ -2,9 +2,34 @@ package com.koala.model;
 
 public class FinanceProduct {
     private Integer id;
+
     private String name;
+
     private String source;
-    private Double profit;
+
+    private String rawUrl;
+
+    private Integer investCycle;
+
+    private Integer riskScore;
+
+    private Integer lowestAmount;
+
+    private String financeProductcol1;
+
+    private Float expectedProfitRate;
+
+    private String expectedProfitName;
+
+    private String investField;
+
+    private String productTypeLabel;
+
+    private String channelNameShort;
+
+    private String channelName;
+
+    private String channelUrl;
 
     public Integer getId() {
         return id;
@@ -19,7 +44,7 @@ public class FinanceProduct {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
 
     public String getSource() {
@@ -27,14 +52,102 @@ public class FinanceProduct {
     }
 
     public void setSource(String source) {
-        this.source = source;
+        this.source = source == null ? null : source.trim();
     }
 
-    public Double getProfit() {
-        return profit;
+    public String getRawUrl() {
+        return rawUrl;
     }
 
-    public void setProfit(Double profit) {
-        this.profit = profit;
+    public void setRawUrl(String rawUrl) {
+        this.rawUrl = rawUrl == null ? null : rawUrl.trim();
+    }
+
+    public Integer getInvestCycle() {
+        return investCycle;
+    }
+
+    public void setInvestCycle(Integer investCycle) {
+        this.investCycle = investCycle;
+    }
+
+    public Integer getRiskScore() {
+        return riskScore;
+    }
+
+    public void setRiskScore(Integer riskScore) {
+        this.riskScore = riskScore;
+    }
+
+    public Integer getLowestAmount() {
+        return lowestAmount;
+    }
+
+    public void setLowestAmount(Integer lowestAmount) {
+        this.lowestAmount = lowestAmount;
+    }
+
+    public String getFinanceProductcol1() {
+        return financeProductcol1;
+    }
+
+    public void setFinanceProductcol1(String financeProductcol1) {
+        this.financeProductcol1 = financeProductcol1 == null ? null : financeProductcol1.trim();
+    }
+
+    public Float getExpectedProfitRate() {
+        return expectedProfitRate;
+    }
+
+    public void setExpectedProfitRate(Float expectedProfitRate) {
+        this.expectedProfitRate = expectedProfitRate;
+    }
+
+    public String getExpectedProfitName() {
+        return expectedProfitName;
+    }
+
+    public void setExpectedProfitName(String expectedProfitName) {
+        this.expectedProfitName = expectedProfitName == null ? null : expectedProfitName.trim();
+    }
+
+    public String getInvestField() {
+        return investField;
+    }
+
+    public void setInvestField(String investField) {
+        this.investField = investField == null ? null : investField.trim();
+    }
+
+    public String getProductTypeLabel() {
+        return productTypeLabel;
+    }
+
+    public void setProductTypeLabel(String productTypeLabel) {
+        this.productTypeLabel = productTypeLabel == null ? null : productTypeLabel.trim();
+    }
+
+    public String getChannelNameShort() {
+        return channelNameShort;
+    }
+
+    public void setChannelNameShort(String channelNameShort) {
+        this.channelNameShort = channelNameShort == null ? null : channelNameShort.trim();
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName == null ? null : channelName.trim();
+    }
+
+    public String getChannelUrl() {
+        return channelUrl;
+    }
+
+    public void setChannelUrl(String channelUrl) {
+        this.channelUrl = channelUrl == null ? null : channelUrl.trim();
     }
 }
